723713c2abf1fd22c278a2c523e2f1e046d34f68
ivanj26 <ivanjth26@gmail.com>
2019-11-06 22:19:41 +0700

    separate generate ticket from create order

--- TaskServices/workers/order.js
+++ TaskServices/workers/order.js
@@ -15,11 +15,10 @@ const axiosConfig = {
 
 let instance;
 
+/** Create order */
 worker.subscribe('create-order', async function({ task, taskService }) {
   // Get a process variable
   const user_id = task.variables.get('user_id');
-  const event_id = task.variables.get('event_id');
-  const seat_num = task.variables.get('seat_num');
   const price = task.variables.get('price');
   const quantity = task.variables.get('quantity');
 
@@ -39,24 +38,9 @@ worker.subscribe('create-order', async function({ task, taskService }) {
         const order_id = res.data.id;
         
         console.log('Order created');
-
-        const ticketBody = JSON.stringify({
-          order_id,
-          event_id,
-          seat_num,
-          price,
-          quantity
-        });
-
-        await instance.post(`${restUrl}/ticket`, ticketBody)
-          .then((_) => {
-            processVariables.set('order_id', order_id);
-
-            console.log('Ticket created');
-          })
-          .catch((err) => {
-            console.log(err);
-          })
+        
+        processVariables.set('order_id', order_id);
+        processVariables.set('total_price', price * quantity);
       })
       .catch((err) => {
         console.log(err);
@@ -68,3 +52,61 @@ worker.subscribe('create-order', async function({ task, taskService }) {
     await taskService.complete(task, processVariables);
   }
 });
+
+/**
+ * Receive Payment
+ */
+
+worker.subscribe('receive-payment', async function({ task, taskService }) {
+  const order_id = task.variables.get("order_id");
+  const success = task.variables.get("success");
+
+  instance = axios.create(axiosConfig);
+
+  let processVariables = new Variables();
+
+  try {
+    if (success) {
+      processVariables.set("order_id", order_id);
+    } else {
+      processVariables.set("error_message", "Gagal melakukan pembayaran!");
+    }
+  } catch (e) {
+    console.log(e);
+  } finally {
+    await taskService.complete(task, processVariables);
+  }
+});
+
+/** Generate ticket */
+worker.subscribe('generate-ticket', async function({task, taskService}) {
+  const order_id = task.variables.get('order_id');
+  const event_id = task.variables.get('event_id');
+  const seat_num = task.variables.get('seat_num');
+  const price = task.variables.get('price');
+  const quantity = task.variables.get('quantity');
+
+  const ticketBody = JSON.stringify({
+    order_id,
+    event_id,
+    seat_num,
+    price,
+    quantity
+  });
+
+  instance = axios.create(axiosConfig);
+
+  try {
+    await instance.post(`${restUrl}/ticket`, ticketBody)
+    .then((_) => {
+      console.log('Ticket created');
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  } catch (e) {
+    console.log(e);
+  } finally {
+    await taskService.complete(task, processVariables);
+  }
+})
